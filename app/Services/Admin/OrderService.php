3737cc8af2d3528886d5c87762796645d9897809
mahmouderfan95 <mahmoudesmailgamal0@gmail.com>
2026-07-23 11:51:25 +0300

    update order status

--- app/Services/Admin/OrderService.php
+++ app/Services/Admin/OrderService.php
@@ -1,10 +1,12 @@
 <?php
 namespace App\Services\Admin;
+use App\Enums\OrderStatusEnum;
 use App\Http\Resources\Admin\Orders\OrderCollection;
 use App\Http\Resources\Admin\Orders\OrderResource;
 use App\Repositories\Admin\OrderRepository;
 use App\Traits\ApiResponseAble;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class OrderService
@@ -38,6 +40,44 @@ class OrderService
         }catch (\Exception $exception){
             Log::error('error of show order '.$exception->getMessage());
             return $this->ApiErrorResponse([], 'something went wrong');
+        }
+    }
+    public function updateStatus($id,$request) : JsonResponse
+    {
+        try{
+            DB::beginTransaction();
+            $order = $this->orderRepository->getModelById($id);
+            if (!$order) {
+                return $this->notFoundResponse();
+            }
+            if ($order->status === OrderStatusEnum::CANCELED && $request->status !== OrderStatusEnum::REFUNDED) {
+                return $this->ApiErrorResponse([],'Cannot change status from cancelled unless refunded.');
+            }
+            if (in_array($order->status, [OrderStatusEnum::DELIVERED, OrderStatusEnum::RETURNED])) {
+                return $this->ApiErrorResponse([], 'Completed orders cannot be updated.');
+            }
+            $order->status = $request->status;
+            $order->save();
+
+            $order->items()->update([
+                'status' => $request->status
+            ]);
+//            $order->user->notify(
+//                new OrderStatusUpdated(
+//                    'Order Status Updated',
+//                    "Your order #$order->id status changed to: {$order->status}."
+//                )
+//            );
+            DB::commit();
+
+            return $this->ApiSuccessResponse(
+                OrderResource::make($order),
+                'Order update status successfully'
+            );
+        }catch (\Exception $exception){
+            DB::rollBack();
+            Log::error('error of update status' . $exception->getMessage());
+            return $this->ApiErrorResponse([],'something went wrong');
         }
     }
     public function destroy($id) : JsonResponse
